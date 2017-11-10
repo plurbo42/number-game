21e880f11a88deb9f52d2c9323e9364b75e0480b
Paige Scheunemann <pls927@gmail.com>
2017-11-10 16:48:50 -0600

    more

--- script.js
+++ script.js
@@ -17,16 +17,18 @@ $(document).ready(onReady);
 function onReady() {
     $('#gamebox').css('border', 'solid', 'black');
     $('#gamebox').append('<button id="newGameButton">New Game</button>');
-    $('#gamebox').css('border-radius', '15%');
+    $('#gamebox').css('border-radius', '25%');
     $('#gamebox').css('width', '500px');
     $('#gamebox').css('text-align', 'center');
     $('#gamebox').append('<p id="clue"><p>');
+    // $('#gamebox').append('<p id="counter">Guess Counter:' + guessCounter + '<p>'); //commenting out Guess Count for now
     $('#gamebox').on('click', '#submit', submitGuess);
     $('#gamebox').on('click', '#newGameButton', newGame);
-    //$('#gamebox').on('keypress', submitGuess); //this is causing some weird behaviour and likely needs a keybinding to work as expected
+    //$('#gamebox').on('keypress', submitGuess); //this is causing some weird behaviour and likely needs a keybinding to work as expected. Removing for now 
 }
 
-var randomNumber = null
+var randomNumber = null;
+// var guessCounter = 0;
 
 function newGame() {
     console.log('in newGame');
@@ -36,12 +38,22 @@ function newGame() {
     $('#newGameButton').replaceWith('<button id="submit">Submit</button>');
     $('#gamebox').css('background-color', 'royalblue')
     $('#clue').replaceWith('<p id="clue"><p>')
+    // guessCounter = 0;
+    // $('#counter').replaceWith('<p id="counter">Guess Counter: <p>');
 }
 
 function submitGuess() {
+    // guessCounter ++
+    // $('#counter').replaceWith('<p id="counter">Guess Counter:' + guessCounter + '<p>');
     var guessedNumber = $('#numberGuess').val();
     console.log('Guessed number is: ', guessedNumber, 'Random number is: ', randomNumber);
-    if(guessedNumber > randomNumber){
+    if(guessedNumber == ''){
+        $('#gamebox').css('background-color', 'red');
+        $('#clue').replaceWith('<p id="clue">YOU DIDN\'T ENTER ANYTHING<p>') //error handling
+    }else if(guessedNumber > 10 || guessedNumber < 1){
+        $('#gamebox').css('background-color', 'red');
+        $('#clue').replaceWith('<p id="clue">READ THE DIRECTIONS<p>') //error handling
+    }else if(guessedNumber > randomNumber){
         $('#gamebox').css('background-color', 'orange')
         $('#clue').replaceWith('<p id="clue">TOO BIG<p>')
     }else if(guessedNumber < randomNumber){
@@ -52,7 +64,14 @@ function submitGuess() {
         $('#clue').replaceWith('<p id="clue">DING DING DING!<p>')
         $('#submit').replaceWith('<button id="newGameButton">New Game</button>');
         $('#numberGuess').remove()
+    }else if(guessedNumber == ''){
+        $('#gamebox').css('background-color', 'red');
+        $('#clue').replaceWith('<p id="clue">YOU DIDN\'T ENTER ANYTHING<p>') //error handling for non-numeric input
     }else{
         $('#gamebox').css('background-color', 'red');
         $('#clue').replaceWith('<p id="clue">THAT\'S NO NUMBER<p>')}
+    //if/else complete
+
+   //reset input complete
+        $('#numberGuess').val('');
     }
